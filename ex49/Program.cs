f9cd3b1fc5014cbfc38ed9ec8bea4057ac7ec072
lilYummy228 <142146352+lilYummy228@users.noreply.github.com>
2023-11-20 10:45:14 +0500

    Добавьте файлы проекта.

---
+++ ex49/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex49
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+
+            }
+        }
+    }
+
+    class Aquarium
+    {
+        private List<Fish> _fishes = new List<Fish>();
+    }
+
+    class Fish
+    {
+        public int Name { get; private set; }
+        public int Age { get; private set; }
+
+        public void CreateFish()
+        {
+            char fishMark = 'Ю';
+        }
+    }
+}
